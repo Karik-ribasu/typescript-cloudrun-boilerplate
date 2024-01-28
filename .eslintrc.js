6b1a949a0bbb590126fbbb4e4747fe93dd2e6e47
karik-ribasu <mechamocaique@gmail.com>
2024-01-28 13:07:33 -0300

    feat: add simple build workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   root: true,
-  parserOptions:{
-    project: './tsconfig.json'
+  parserOptions: {
+    project: './tsconfig.json',
   },
-  
+  rules: {
+    'no-unused-vars': 'off',
+    'prettier/prettier': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
